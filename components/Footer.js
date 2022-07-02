812226b7907cc23514390b0c4292780963913dd3
gitradonz <63265564+gitradonz@users.noreply.github.com>
2022-07-02 11:57:07 +0200

    Fixed class to className

--- components/Footer.js
+++ components/Footer.js
@@ -1,26 +1,26 @@
 import { motion } from "framer-motion";
 
 const Footer = () => {
-	return (
-		<motion.footer
-			whileInView={{ opacity: 1 }}
-			initial={{ opacity: 0.5 }}
-			transition={{ duration: 0.8 }}
-			class="p-4 shadow-md flex align-items-center justify-content-center bg-gray-800"
-		>
-			<span class="text-sm text-gray-500">
-				© 2022{" · "}
-				All Rights Reserved ·{" "}
-				<a
-					href="https://twitter.com/radonzStudio"
-					target={"_blank"}
-					class="hover:underline"
-				>
-					@radonzStudio
-				</a>
-			</span>
-		</motion.footer>
-	);
+  return (
+    <motion.footer
+      whileInView={{ opacity: 1 }}
+      initial={{ opacity: 0.5 }}
+      transition={{ duration: 0.8 }}
+      className="p-4 shadow-md flex align-items-center justify-content-center bg-gray-800"
+    >
+      <span className="text-sm text-gray-500">
+        © 2022{" · "}
+        All Rights Reserved ·{" "}
+        <a
+          href="https://twitter.com/radonzStudio"
+          target={"_blank"}
+          className="hover:underline"
+        >
+          @radonzStudio
+        </a>
+      </span>
+    </motion.footer>
+  );
 };
 
 export default Footer;
