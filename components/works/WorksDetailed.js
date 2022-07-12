bf244294c7a8d9f5ab2a7585d393ff4bc946c528
Pablo Rios <pablo.rios@lobocom.es>
2022-07-12 10:05:28 +0200

    Refactorized works/projects

--- components/works/WorksDetailed.js
+++ components/works/WorksDetailed.js
@@ -1,5 +1,8 @@
 import { motion } from "framer-motion";
 import WorksSection from "./WorksSection";
+import { allWorks } from "../../works/works";
+
+const [jsProjects, javaProjects, csharpProjects, kotlinProjects] = allWorks;
 
 const WorksDetailed = () => {
 	return (
@@ -38,7 +41,7 @@ const WorksDetailed = () => {
 						subtitle="Mobile ( AndroidStudio )"
 						title="KOTLIN"
 						description="I used Kotlin to develop mobile applications with AndroidStudio."
-						projects={worksMockup}
+						projects={kotlinProjects}
 					/>
 				</div>
 				<div className="OTHER">
@@ -46,97 +49,12 @@ const WorksDetailed = () => {
 						subtitle="Fullstack"
 						title="OTHER"
 						description="I used Kotlin to develop mobile applications with AndroidStudio."
-						projects={worksMockup}
+						projects={kotlinProjects}
 					/>
 				</div>
 			</motion.div>
 		</div>
 	);
 };
-
-const worksMockup = [
-	{
-		title: "Facilisis gravida neque",
-		description:
-			"Potenti nullam ac tortor vitae. Aliquet bibendum enim facilisis gravida neque convallis a cras semper.",
-		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
-	},
-	{
-		title: "Facilisis gravida neque",
-		description:
-			"Potenti nullam ac tortor vitae. Aliquet bibendum enim facilisis gravida neque convallis a cras semper.",
-		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
-	},
-	{
-		title: "Facilisis gravida neque",
-		description:
-			"Potenti nullam ac tortor vitae. Aliquet bibendum enim facilisis gravida neque convallis a cras semper.",
-		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
-	},
-	{
-		title: "Facilisis gravida neque",
-		description:
-			"Potenti nullam ac tortor vitae. Aliquet bibendum enim facilisis gravida neque convallis a cras semper.",
-		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
-	},
-];
-const javaProjects = [
-	{
-		title: "LinkTracker",
-		description:
-			"Application to track links in a website. It uses a multi-thread system and JavaFX to show the results.",
-		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
-	},
-	{
-		title: "FlightsFX",
-		description: "Desktop application to track Flights shedule.",
-		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
-	},
-	{
-		title: "CurrencyConverter",
-		description:
-			"Simple desktop application to convert currencies. It also uses JavaFX for the UI.",
-		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
-	},
-	{
-		title: "HeroesFight",
-		description:
-			"Application to make random fights between heroes. It uses console to show the results.",
-		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
-	},
-];
-const jsProjects = [
-	{
-		title: "CRM - Dashboard",
-		description:
-			"Web to manage clients and their products. Has a login system, API connections, MySQL database auth and performance features on the UI.",
-		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
-	},
-	{
-		title: "WebViewer",
-		description:
-			"Mobile app developed to wrap a PWA in a WebView component to give push/local notifications and login system to iOS/Android.",
-		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
-	},
-	{
-		title: "ePub Reader",
-		description:
-			"Web app developed to read ePub books. It has highlighting, search system, dictionary API and other features",
-		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
-	},
-	{
-		title: "Test App",
-		description:
-			"Mobile app developed to implement and test features. It has QR code scanning, camera, file management, logins, pdf reader, biometric authentication...",
-		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
-	},
-];
-const csharpProjects = [
-	{
-		title: "HuntZ",
-		description: "",
-		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
-	},
-];
 
 export default WorksDetailed;
---
+++ works/works.js
@@ -0,0 +1,91 @@
+const kotlinProjects = [
+	{
+		title: "Facilisis gravida neque",
+		description:
+			"Potenti nullam ac tortor vitae. Aliquet bibendum enim facilisis gravida neque convallis a cras semper.",
+		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
+	},
+	{
+		title: "Facilisis gravida neque",
+		description:
+			"Potenti nullam ac tortor vitae. Aliquet bibendum enim facilisis gravida neque convallis a cras semper.",
+		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
+	},
+	{
+		title: "Facilisis gravida neque",
+		description:
+			"Potenti nullam ac tortor vitae. Aliquet bibendum enim facilisis gravida neque convallis a cras semper.",
+		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
+	},
+	{
+		title: "Facilisis gravida neque",
+		description:
+			"Potenti nullam ac tortor vitae. Aliquet bibendum enim facilisis gravida neque convallis a cras semper.",
+		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
+	},
+];
+const javaProjects = [
+	{
+		title: "LinkTracker",
+		description:
+			"Application to track links in a website. It uses a multi-thread system and JavaFX to show the results.",
+		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
+	},
+	{
+		title: "FlightsFX",
+		description: "Desktop application to track Flights shedule.",
+		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
+	},
+	{
+		title: "CurrencyConverter",
+		description:
+			"Simple desktop application to convert currencies. It also uses JavaFX for the UI.",
+		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
+	},
+	{
+		title: "HeroesFight",
+		description:
+			"Application to make random fights between heroes. It uses console to show the results.",
+		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
+	},
+];
+const jsProjects = [
+	{
+		title: "CRM - Dashboard",
+		description:
+			"Web to manage clients and their products. Has a login system, API connections, MySQL database auth and performance features on the UI.",
+		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
+	},
+	{
+		title: "WebViewer",
+		description:
+			"Mobile app developed to wrap a PWA in a WebView component to give push/local notifications and login system to iOS/Android.",
+		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
+	},
+	{
+		title: "ePub Reader",
+		description:
+			"Web app developed to read ePub books. It has highlighting, search system, dictionary API and other features",
+		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
+	},
+	{
+		title: "Test App",
+		description:
+			"Mobile app developed to implement and test features. It has QR code scanning, camera, file management, logins, pdf reader, biometric authentication...",
+		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
+	},
+];
+const csharpProjects = [
+	{
+		title: "HuntZ",
+		description: "",
+		image: "https://altertecnia.com/wp-content/uploads/oee-4.png",
+	},
+];
+
+export const allWorks = [
+	jsProjects,
+	javaProjects,
+	csharpProjects,
+	kotlinProjects,
+];
