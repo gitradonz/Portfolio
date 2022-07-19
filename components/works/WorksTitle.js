4ea13a00c7fa7d22a27edffc8744b72adb52f3e3
gitradonz <63265564+gitradonz@users.noreply.github.com>
2022-07-19 11:40:42 +0200

    Fixed works title ( viewport once )

--- components/works/WorksTitle.js
+++ components/works/WorksTitle.js
@@ -6,6 +6,7 @@ const WorksTitle = () => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 0.7 }}
+      viewport={{ once: true }}
       className="bg-yellow-100 text-center text-7xl "
     >
       <div className="WORKSTITLE">WORKS</div>
